0b8ccfda0ab703ef644393e79fa474cc4cc406a0
Aaron Christiansen <125aaron@gmail.com>
2019-02-14 12:54:33 -0700

    fixed pokemonlist

--- src/pokemon/controller/PokedexController.java
+++ src/pokemon/controller/PokedexController.java
@@ -1,17 +1,37 @@
 package pokemon.controller;
 
+import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
 
+import pokemon.model.Cyndaquil;
+import pokemon.model.Magikarp;
+import pokemon.model.Mudkip;
 import pokemon.model.Pokemon;
+import pokemon.model.Psyduck;
+import pokemon.model.Squirtle;
 import pokemon.view.PokedexFrame;
 
 public class PokedexController
 {
 	private PokedexFrame appFrame;
 
+	private ArrayList<Pokemon> pokemonList;
+
 	public PokedexController()
 	{
+		pokemonList = new ArrayList<Pokemon>();
+		addPokemon();
 		appFrame = new PokedexFrame(this);
+	}
+
+	public void addPokemon()
+	{
+		pokemonList.add(new Psyduck());
+		pokemonList.add(new Cyndaquil());
+		pokemonList.add(new Mudkip());
+		pokemonList.add(new Magikarp());
+		pokemonList.add(new Squirtle());
 	}
 
 	public void start()
@@ -35,47 +55,45 @@ public class PokedexController
 
 	public String[] buildPokedexText()
 	{
-	String [] names =  new String [pokemonList.size()];
-			
-	for(int index = 0; index < pokemonList.size() index++);
-			{
-				names[index]= pokemonList.get(index).getName();
-			}
-			return names;
+		String[] names = new String[pokemonList.size()];
+
+		for (int index = 0; index < pokemonList.size(); index++)
+		{
+			names[index] = pokemonList.get(index).getName();
+		}
+		return names;
 	}
-	
+
 	public boolean validInt(String maybeInt)
 	{
 		boolean isValid = false;
-				
+
 		try
 		{
 			Integer.parseInt(maybeInt);
 			isValid = true;
-		}
-		catch (NumberFormatException error)
+		} catch (NumberFormatException error)
 		{
 			JOptionPane.showMessageDialog(null, "You need to type in a number :/");
 		}
 		return isValid;
 	}
-	
+
 	public boolean validDouble(String mightbeDouble)
 	{
 		boolean isValid = false;
-		
+
 		try
 		{
 			Double.parseDouble(mightbeDouble);
 			isValid = true;
-		}
-		catch (NumberFormatException error)
+		} catch (NumberFormatException error)
 		{
-			JOptionPane.showMessageDialog(null,  "Type in a decimal value aka a number with a . in it");
+			JOptionPane.showMessageDialog(null, "Type in a decimal value aka a number with a . in it");
 		}
-		
+
 		return isValid;
-	
+
 	}
 
 }
