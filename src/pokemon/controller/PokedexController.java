e4e78c25151d18f2d3eaddafeab8c408ec7c296f
Aaron Christiansen <125aaron@gmail.com>
2019-02-14 11:46:56 -0700

    added images and classes for pokemon/class

--- src/pokemon/controller/PokedexController.java
+++ src/pokemon/controller/PokedexController.java
@@ -1,18 +1,45 @@
 package pokemon.controller;
 
+import pokemon.model.Pokemon;
 import pokemon.view.PokedexFrame;
 
-public class PokedexController 
+public class PokedexController
 {
 	private PokedexFrame appFrame;
-	
+
 	public PokedexController()
 	{
 		appFrame = new PokedexFrame(this);
 	}
-	public void start() {
+
+	public void start()
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
+
+	public void updatePokemon(int index, String[] data)
+	{
+		if (data.length == 5)
+		{
+			Pokemon current = pokemonList.get(index);
+			current.setAttackPoints(Integer.parseInt(data[0]));
+			current.setEnhancementModifier(Double.parseDouble(data[1]));
+			current.setHealthPoints(Integer.parseInt(data[2]));
+			current.setName(data[3]);
+			current.setCanEvolve(Boolean.parseBoolean(data[4]));
+		}
+	}
+
+public String[] buildPokedexText()
+{
+	String [] names =  new String [pokemonList.size()];
+			
+	for(int index = 0; index < pokemonList.size() index++);
+			{
+				names[index]= pokemonList.get(index).getName();
+			}
+			return names;
+}
 
 }
--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -32,6 +32,7 @@ import java.awt.Color;
 		private JLabel attackLabel;
 		private JLabel enhanceLabel;
 		private JLabel healthLabel;
+		private JLabel healthLabel_1;
 		private JLabel imageLabel;
 		
 		public PokedexPanel(PokedexController appController)
@@ -42,25 +43,37 @@ import java.awt.Color;
 			
 			numberField = new JTextField("0");
 			nameField = new JTextField("My pokename");
-			appLayout.putConstraint(SpringLayout.NORTH, nameField, 29, SpringLayout.NORTH, this);
-			appLayout.putConstraint(SpringLayout.WEST, nameField, 313, SpringLayout.WEST, this);
+
 			evolveField = new JTextField("false");
+
 			attackField = new JTextField("0");
 			enhancementField = new JTextField("0");
 			healthField = new JTextField("0");
+			appLayout.putConstraint(SpringLayout.WEST, healthField, 0, SpringLayout.WEST, numberField);
+
 			
 			
 			
 			healthLabel = new JLabel("This pokemans health is");
 			numberLabel = new JLabel("This pokmans number is");
+
 			
 			evolveLabel = new JLabel("this pokeman can evolve");
+
 			attackLabel = new JLabel("this pokemans attack is");
+			
 			enhanceLabel = new JLabel("this pokeman can be enhanced");
-			healthLabel = new JLabel("this pokemans health");
+			
+			healthLabel_1 = new JLabel("this pokemans health");
+			appLayout.putConstraint(SpringLayout.NORTH, healthField, -5, SpringLayout.NORTH, healthLabel_1);
 			imageLabel = new JLabel();
+			changeButton = new JButton("Click here to change the pokevalues");
+			pokedexDropdown = new JComboBox<String>();
+			appLayout.putConstraint(SpringLayout.NORTH, pokedexDropdown, 83, SpringLayout.NORTH, this);
+			appLayout.putConstraint(SpringLayout.EAST, pokedexDropdown, -288, SpringLayout.EAST, this);
+
 			
-			
+			setupDropdown();
 			setupScrollPane();
 			setupPanel();
 			setupLayout();
@@ -68,12 +81,44 @@ import java.awt.Color;
 			
 		}
 		
-		
+		private void setupDropdown()
+		{
+			DefaultComboBoxModel<String> temp = new DefaultComboBoxModel<String>(appLayout.buildPokedexText());
+			pokedexDropdown.setModel(temp);
+		}
 		
 		private void setupLayout()
 		{
-		
+			appLayout.putConstraint(SpringLayout.NORTH, nameField, 29, SpringLayout.NORTH, this);
+			appLayout.putConstraint(SpringLayout.WEST, nameField, 313, SpringLayout.WEST, this);
 			
+			appLayout.putConstraint(SpringLayout.EAST, numberField, 0, SpringLayout.EAST, evolveField);
+			
+			appLayout.putConstraint(SpringLayout.NORTH, numberField, 15, SpringLayout.NORTH, numberLabel);
+			appLayout.putConstraint(SpringLayout.NORTH, numberLabel, 114, SpringLayout.SOUTH, nameField);
+			appLayout.putConstraint(SpringLayout.EAST, numberLabel, 0, SpringLayout.EAST, nameField);
+			
+			appLayout.putConstraint(SpringLayout.NORTH, evolveField, -5, SpringLayout.NORTH, evolveLabel);
+			appLayout.putConstraint(SpringLayout.WEST, evolveField, 142, SpringLayout.EAST, evolveLabel);
+			appLayout.putConstraint(SpringLayout.NORTH, evolveLabel, 204, SpringLayout.SOUTH, nameField);
+			appLayout.putConstraint(SpringLayout.EAST, evolveLabel, 0, SpringLayout.EAST, nameField);
+			appLayout.putConstraint(SpringLayout.SOUTH, numberLabel, -33, SpringLayout.NORTH, evolveLabel);
+			
+			appLayout.putConstraint(SpringLayout.NORTH, attackField, -5, SpringLayout.NORTH, attackLabel);
+			appLayout.putConstraint(SpringLayout.WEST, attackField, 164, SpringLayout.EAST, attackLabel);
+			appLayout.putConstraint(SpringLayout.EAST, attackLabel, 0, SpringLayout.EAST, nameField);
+			appLayout.putConstraint(SpringLayout.NORTH, attackLabel, 74, SpringLayout.SOUTH, evolveLabel);
+			
+			appLayout.putConstraint(SpringLayout.NORTH, enhancementField, -5, SpringLayout.NORTH, enhanceLabel);
+			appLayout.putConstraint(SpringLayout.WEST, enhancementField, 164, SpringLayout.EAST, enhanceLabel);
+			appLayout.putConstraint(SpringLayout.EAST, enhanceLabel, 0, SpringLayout.EAST, nameField);
+			appLayout.putConstraint(SpringLayout.NORTH, enhanceLabel, 74, SpringLayout.SOUTH, attackLabel);
+			
+			appLayout.putConstraint(SpringLayout.SOUTH, healthLabel_1, -23, SpringLayout.NORTH, numberLabel);
+			appLayout.putConstraint(SpringLayout.EAST, healthLabel_1, 0, SpringLayout.EAST, nameField);
+			
+			appLayout.putConstraint(SpringLayout.NORTH, imageLabel, 164, SpringLayout.NORTH, this);
+			appLayout.putConstraint(SpringLayout.WEST, imageLabel, 0, SpringLayout.WEST, this);
 		}
 		
 		private void setupListeners()
@@ -98,17 +143,14 @@ import java.awt.Color;
 			this.add(attackField);
 			this.add(enhancementField);
 			this.add(healthField);
-			
-			
-			
-			this.add(healthLabel);
-			this.add(numberLabel);
-			
+			this.add(healthLabel_1);
+			this.add(numberLabel);	
 			this.add(evolveLabel);
 			this.add(attackLabel);
 			this.add(enhanceLabel);
-			this.add(healthLabel);
+			this.add(healthLabel_1);
 			this.add(imageLabel);
+			this.add(pokedexDropdown);
 			
 		}
 	}
